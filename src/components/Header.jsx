945be6c706265ad69e00a382cf88acd349c8304e
jakubrejch <jrejch@citizengo.net>
2025-03-07 17:52:28 +0100

    add auth context to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,19 +5,22 @@ import {
   Navigation
 } from '../assets/styles/Header.styled';
 import { MainContext } from '../context/MainContext';
+import { AuthContext } from '../context/AuthContext';
 import LanguageSelector from './Language_selector';
 import Login from './Home/Login';
 import { Link } from 'react-router-dom';
 
 const Header = ({ openModal, closeModal, modalIsOpen }) => {
   const { t } = useContext(MainContext);
+  const { auth } = useContext(AuthContext);
+  
   return (
     <HeaderStyle>
       <Logo />
       <Navigation>
         <Link to="/" >{t('header.navbar.portfolio')}</Link>
         <Link to="/blog" >{t('header.navbar.blog')}</Link>
-        <Link to="/admin" >{t('header.navbar.admin')}</Link>
+        {auth && <Link to="/admin" >{t('header.navbar.admin')}</Link>}
       </Navigation>
         <Login openModal={openModal} closeModal={closeModal} modalIsOpen={modalIsOpen} />
         <LanguageSelector />
